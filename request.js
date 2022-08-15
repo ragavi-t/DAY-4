800f1ae8c18ca309b35dab8dda073b1c7384eef5
RAGAVI T <110911541+ragavi-t@users.noreply.github.com>
2022-08-15 15:33:27 +0530

    Add files via upload

---
+++ request.js
@@ -0,0 +1,16 @@
+//make a http request
+//create an instance of the request
+let xhr = new XMLHttpRequest();
+//Open your request
+xhr.open("GET","https://restcountries.com/v3.1/all");
+//onload event (server will listen to request and process data )
+xhr.onload=function(){console.log("************Response Received************");console.log(this.responseText);
+//raw data
+let data=JSON.parse(xhr.responseText)
+//converted data into object
+console.log(data);
+//Loop throigh the data and print it on the console
+for(let i=0;i<data.length;i++){console.log(`Country:${data[i].country }Region:${data[i].region}Subregion:${data[i].subregion} Population${data[i].population}`)}}
+//send the request
+xhr.send();
+// fetch>crud operations/
